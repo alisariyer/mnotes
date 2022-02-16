e5c61ebf4d17e7382c070cf4c8d235fc3418d736
Ali Sariyer <alisariyer@gmail.com>
2022-02-16 23:05:02 +0000

    Add main components and in index.css file write custom properties and reset styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import Notes from "./components/Notes";
+import Buttons from "./components/Buttons"
 
 export default function App() {
-    return (
-        <h1>Hello world</h1>
-    )
+  return (
+    <div className="container">
+      <Header />
+      <Notes />
+      <Buttons />
+    </div>
+  );
 }
